46ea41bbf98a36b4ad43c742fae7e81f5b6a3d07
Ryan <Ryan-rgbrowni@usc.edu>
2019-02-09 13:09:51 -0800

    First commit for pushing up the homework

---
+++ pandashw.py
@@ -0,0 +1,9 @@
+# Dependencies and Setup
+import pandas as pd
+import numpy as np
+
+# Raw data file
+file_to_load = "Resources/purchase_data.csv"
+
+# Read purchasing file and store into pandas data frame
+purchase_data = pd.read_csv(file_to_load)
